9ec47f89ba57bcbcb5e0599aab85989bdbd7d591
WaronLimsakul <poonro@gmail.com>
2025-02-24 00:51:14 -0500

    c2 done

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"log"
+	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -52,11 +55,22 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	// DON'T media type from r.Header.Get(), it's gonna give you
 	// multi-part form instead of those "image/png", "image/jpg"
 	// you must get it from header of the parsed file
-	mediaType := reqFileHeader.Header.Get("Content-Type")
+	fileContentType := reqFileHeader.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(fileContentType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse media type", err)
+		return
+	}
+
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		const msg = "Only jpg and png accepted"
+		respondWithError(w, http.StatusBadRequest, msg, fmt.Errorf("media type: %s", mediaType))
+		return
+	}
 
 	videoMetaData, err := cfg.db.GetVideo(videoID)
 	if err != nil {
-		respondWithError(w, http.StatusNotFound, "Couldn't find video metadata", err)
+		respondWithError(w, http.StatusNotFound, "Couldn't find video metadata", fmt.Errorf(""))
 		return
 	}
 
@@ -71,9 +85,17 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	thumbnailPath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoMetaData.ID, thumbnailExtension))
+	// a chunk of 32 bytes, no pointer
+	var rand32Bytes [32]byte
+	// create a slice pointing to the array
+	rand.Read(rand32Bytes[:])
+	// encode it to use in url
+	thumbnailName := base64.RawURLEncoding.EncodeToString(rand32Bytes[:])
+
+	thumbnailPath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", thumbnailName, thumbnailExtension))
 	thumbnailFile, err := os.Create(thumbnailPath)
 	if err != nil {
+		fmt.Printf("err: %s\n", err)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create file in disk", err)
 		return
 	}
@@ -87,7 +109,8 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, mediaType)
+	// don't forget : media type = image/png, but extension = png
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/%s", cfg.port, thumbnailPath)
 	videoMetaData.ThumbnailURL = &thumbnailURL
 
 	err = cfg.db.UpdateVideo(videoMetaData)
